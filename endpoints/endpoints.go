25d9d9433d6a772a64a9611fbc27394c06bede77
Zhui <zhoujiahui@bailuntec.com>
2023-08-02 14:11:01 +0800

    add outputEndpoint

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -2,6 +2,18 @@ package endpoints
 
 import "github.com/Zhui-CN/pipeflow/tasks"
 
+// InputEndpoint get a message from queue and parse message into a task
 type InputEndpoint interface {
 	Get() tasks.Task
+}
+
+// OutputEndpoint parse task into message and put into queue
+type OutputEndpoint interface {
+	Put(*Output)
+}
+
+// Output parse handle result
+type Output struct {
+	Task   tasks.Task
+	Params any
 }
